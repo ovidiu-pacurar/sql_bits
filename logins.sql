b553116355bfc762b82762426a017c87040729e6
ovidiu-pacurar <ovidiu.pacurar@gmail.com>
2018-09-21 17:46:00 +0300

    Create logins.sql

---
+++ logins.sql
@@ -0,0 +1,7 @@
+-- CLONE sql logins
+select 
+	name, 
+	command = 'CREATE LOGIN ['+name+'] WITH PASSWORD = '+CONVERT(varchar(max), password_hash, 1)+' HASHED, SID = '+ CONVERT(varchar(max), sid, 1)+',  DEFAULT_DATABASE=['+default_database_name+'], DEFAULT_LANGUAGE=['+default_language_name+'], CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF;'
+	--,*
+from sys.sql_logins
+order by 1
